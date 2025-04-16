d13427b4fb3828628b4716a1caa717c866eae099
hugorouillard <hugorouillard@users.noreply.github.com>
2025-04-16 13:02:53 +0000

    deploy: 871ecd08e2dd2d5ca2ebaacd6e4b9835566957cc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.hugorouillard.dataframe","l":"ConversionUtils"},{"p":"com.github.hugorouillard.dataframe","l":"Dataframe"},{"p":"com.github.hugorouillard.dataframe","l":"Series"}];updateSearchResults();
